db68c4510797c2646e56ecce8e8236691db3cc2e
Vsevolod Romashov <7@7vn.ru>
2017-01-31 12:16:47 +0300

    use transform-ship in transform-options, refactor

--- src/endless_ships/core.clj
+++ src/endless_ships/core.clj
@@ -4,8 +4,54 @@
             [instaparse.core :as insta])
   (:import [java.lang Float Integer]))
 
+(defn all-with-key [key data]
+  (->> data
+       (filter #(= (first %) key))
+       (map (comp vec rest))))
+
+(defn first-with-key [key data]
+  (first (all-with-key key data)))
+
+(defn all-without-key [key data]
+  (->> data
+       (filter #(not= (first %) key))
+       (map (comp vec rest))))
+
+(defn transform-ship [& ship-params]
+  (let [licenses (->> ship-params
+                      (first-with-key :licenses)
+                      (map #(get % 1))
+                      vec)
+        all-attributes (first-with-key :attributes ship-params)
+        weapon (->> all-attributes
+                    (first-with-key :weapon)
+                    (map (comp vec rest))
+                    (into {}))
+        other-attributes (->> all-attributes
+                              (all-without-key :weapon)
+                              (into {}))
+        outfits (->> ship-params
+                     (first-with-key :outfits)
+                     (map (fn [[_ name quantity]]
+                            [name (or quantity 1)]))
+                     (into {}))
+        other (->> ship-params
+                   (all-with-key :any-detail)
+                   (reduce (fn [details [detail-name & detail]]
+                             (update details
+                                     detail-name
+                                     #(conj (or % []) (vec detail))))
+                           {}))]
+    {:ship-name (first-with-key :ship-name ship-params)
+     :sprite (first-with-key :sprite ship-params)
+     :licenses licenses
+     :attributes (merge {:weapon weapon} other-attributes)
+     :outfits outfits
+     :other other}))
+
 (def transform-options
-  {:string identity
+  {:ship transform-ship
+   :string identity
    :integer #(Integer/parseInt %)
    :float #(Float/parseFloat (str/replace % "," "."))})
 
@@ -35,51 +81,5 @@
                               slurp
                               parse
                               rest)]
-                (map #(conj % [:race race]) ships))))
+                (map #(assoc % :race race) ships))))
        (apply concat)))
-
-(defn all-with-key [key data]
-  (->> data
-       (filter #(= (first %) key))
-       (map (comp vec rest))))
-
-(defn first-with-key [key data]
-  (first (all-with-key key data)))
-
-(defn all-without-key [key data]
-  (->> data
-       (filter #(not= (first %) key))
-       (map (comp vec rest))))
-
-(defn transform-ship [[_ & ship-params]]
-  (let [licenses (->> ship-params
-                      (first-with-key :licenses)
-                      (map #(get % 1))
-                      vec)
-        all-attributes (first-with-key :attributes ship-params)
-        weapon (->> all-attributes
-                    (first-with-key :weapon)
-                    (map (comp vec rest))
-                    (into {}))
-        other-attributes (->> all-attributes
-                              (all-without-key :weapon)
-                              (into {}))
-        outfits (->> ship-params
-                     (first-with-key :outfits)
-                     (map (fn [[_ name quantity]]
-                            [name (or quantity 1)]))
-                     (into {}))
-        other (->> ship-params
-                   (all-with-key :any-detail)
-                   (reduce (fn [details [detail-name & detail]]
-                             (update details
-                                     detail-name
-                                     #(conj (or % []) (vec detail))))
-                           {}))]
-    {:ship-name (first-with-key :ship-name ship-params)
-     :race (first (first-with-key :race ship-params))
-     :sprite (first-with-key :sprite ship-params)
-     :licenses licenses
-     :attributes (merge {:weapon weapon} other-attributes)
-     :outfits outfits
-     :other other}))
