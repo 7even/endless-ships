c8350dc1889dcb30e9d5a41acb42905161d51c4f
Vsevolod Romashov <7@7vn.ru>
2017-02-05 17:48:17 +0300

    add core/generate-json

--- build.boot
+++ build.boot
@@ -1,9 +1,10 @@
 (set-env! :source-paths #{"src"}
           :resource-paths #{"resources"}
           :dependencies '[[org.clojure/clojure "1.8.0"]
-                          [instaparse "1.4.5"]])
+                          [instaparse "1.4.5"]
+                          [cheshire "5.7.0"]])
 
-(require '[endless-ships.core :refer [parse]])
+(require '[endless-ships.core :refer [generate-json]])
 
 (deftask run
   []
--- src/endless_ships/core.clj
+++ src/endless_ships/core.clj
@@ -1,2 +1,16 @@
 (ns endless-ships.core
-  (:require [endless-ships.parser :refer [data]]))
+  (:require [endless-ships.parser :refer [data]]
+            [cheshire.core :refer [generate-string]]))
+
+(def ships-data
+  (->> data
+       (filter #(= (count (:ship-name %)) 1)) ; remove modifications
+       (map (fn [ship]
+              (merge {:ship-name (first (:ship-name ship))
+                      :licenses (:licenses ship)
+                      :race (:race ship)}
+                     (dissoc (:attributes ship) :weapon))))))
+
+(defn generate-json [& {:keys [pretty] :or {pretty true}}]
+  (let [json (generate-string ships-data {:pretty pretty})]
+    (spit "build/data.json" (str json "\n"))))
