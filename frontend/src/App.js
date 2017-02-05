476cfbfa88a2c7c137e73d3a26937683869b420e
Vsevolod Romashov <7@7vn.ru>
2017-02-06 01:35:36 +0300

    add bootstrap and a basic table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Table, Label } from 'react-bootstrap';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 class App extends Component {
@@ -16,6 +18,25 @@ class App extends Component {
     });
   }
 
+  renderLicenses(ship) {
+    return ship.licenses.map(license => (
+      <Label key={license}>{license}</Label>
+    ));
+  }
+
+  renderRows() {
+    return this.state.data.map(ship => (
+      <tr key={ship.name}>
+        <td>{ship.name}</td>
+        <td>{ship.race}</td>
+        <td>{ship.cost}</td>
+        <td>{ship.hull}</td>
+        <td>{ship.shields}</td>
+        <td>{this.renderLicenses(ship)}</td>
+      </tr>
+    ));
+  }
+
   render() {
     if (this.state.isLoading) {
       return (<div className="App">Loading...</div>);
@@ -29,6 +50,21 @@ class App extends Component {
           <p className="App-intro">
             Loaded {this.state.data.length} ships.
           </p>
+          <Table striped bordered condensed hover>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Race</th>
+                <th>Cost</th>
+                <th>Hull</th>
+                <th>Shields</th>
+                <th>Licenses</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.renderRows()}
+            </tbody>
+          </Table>
         </div>
       );
     }
