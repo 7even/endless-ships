2fb213cd621bd218655d44e6b7ab340814f4d733
Vsevolod Romashov <7@7vn.ru>
2017-02-09 00:08:47 +0300

    take only the attributes needed for the table

--- src/endless_ships/core.clj
+++ src/endless_ships/core.clj
@@ -13,14 +13,17 @@
               (merge {:name (first (:shipName ship))
                       :licenses (:licenses ship)
                       :race (:race ship)}
-                     (-> ship :attributes (dissoc :weapon :heatDissipation :drag)))))))
+                     (-> ship :attributes (select-keys [:cost :category :hull :shields :mass
+                                                        :engineCapacity :weaponCapacity :fuelCapacity
+                                                        :outfitSpace :cargoSpace
+                                                        :requiredCrew :bunks])))))))
 
 (defn generate-json [& {:keys [pretty] :or {pretty true}}]
   (let [json (generate-string ships-data {:pretty pretty})]
     (spit "build/data.json" (str json "\n"))))
 
-;;; generate sample data for frontend development
 (comment
+  ;; generate sample data for frontend development
   (let [ships (concat (->> ships-data (filter #(= (:race %) "human")) (take 3))
                       (->> ships-data (filter #(= (:race %) "hai")) (take 2))
                       (->> ships-data (filter #(= (:race %) "korath")) (take 2))
@@ -28,4 +31,9 @@
                       (->> ships-data (filter #(= (:race %) "coalition")) (take 2))
                       (->> ships-data (filter #(= (:race %) "quarg"))))
         json (generate-string ships {:pretty true})]
-    (spit "frontend/public/data.json" (str json "\n"))))
+    (spit "frontend/public/data.json" (str json "\n")))
+  ;; get a list of all possible attribute names
+  (->> ships-data
+       (map keys)
+       (apply concat)
+       (into #{})))
