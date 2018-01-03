907baef0bca77b010a309cedcc7db7ef40b14d5d
Vsevolod Romashov <7@7vn.ru>
2018-01-03 23:50:27 +0300

    rewrite all transformations in endless-ships.parser

--- src/endless_ships/parser.clj
+++ src/endless_ships/parser.clj
@@ -1,5 +1,6 @@
 (ns endless-ships.parser
-  (:require [clojure.java.io :refer [file resource]]
+  (:require [camel-snake-kebab.core :refer [->kebab-case]]
+            [clojure.java.io :refer [file resource]]
             [clojure.string :as str]
             [instaparse.core :as insta])
   (:import [java.lang Float Integer]))
@@ -13,7 +14,7 @@
                    "wanderer ships.txt"
                    "coalition ships.txt"
                    "kestrel.txt"
-                   ;; "pug.txt"
+                   "pug.txt"
                    "outfits.txt"]]
     (map #(-> (str "game/data/" %)
               resource
@@ -26,77 +27,18 @@
        (map (comp vec rest))
        first))
 
-(defn transform-ship [ship-params]
-  (let [name (vec (remove vector? ship-params))
-        sprite (first-with-key "sprite" ship-params)
-        all-attributes (first-with-key "attributes" ship-params)
-        weapon (->> all-attributes
-                    (first-with-key "weapon")
-                    (into {}))
-        licenses (->> all-attributes
-                      (first-with-key "licenses")
-                      (apply concat)
-                      vec)
-        other-attributes (->> all-attributes
-                              (remove #(#{"weapon" "licenses"} (first %)))
-                              (into {}))
-        outfits (->> ship-params
-                     (first-with-key "outfits")
-                     (map (fn [[name quantity]]
-                            [name (or quantity 1)]))
-                     (into {}))
-        other (->> ship-params
-                   (filter (fn [el]
-                             (and (vector? el)
-                                  (not (#{"sprite"
-                                          "attributes"
-                                          "outfits"}
-                                        (first el))))))
-                   (reduce (fn [details [detail-name & detail]]
-                             (update details
-                                     detail-name
-                                     #(conj (or % []) (vec detail))))
-                           {}))]
-    {:type ::ship
-     :ship-name name
-     :sprite sprite
-     :licenses licenses
-     :attributes (merge {:weapon weapon} other-attributes)
-     :outfits outfits
-     :other other}))
-
-(defn transform-outfit [outfit-params]
-  {:type ::outfit
-   :data outfit-params}
-  (let [name (vec (remove vector? outfit-params))
-        thumbnail (first-with-key "thumbnail" outfit-params)
-        category (first-with-key "category" outfit-params)
-        other-attributes (->> outfit-params
-                              (filter #(and (vector? %)
-                                            (not (#{"thumbnail" "category"} (first %)))))
-                              (into {}))]
-    {:type ::outfit
-     :name name
-     :category category
-     :thumbnail thumbnail
-     :attributes other-attributes}))
-
-(defn- transform-data [& data]
-  (map (fn [[type & attributes]]
-         (case type
-           "ship" (transform-ship attributes)
-           "outfit" (transform-outfit attributes)
-           {:type type})
-         ;; {:type type :attributes attributes}
-         )
-       data))
-
 (defn- transform-block [[_ name & args] & child-blocks]
-  (vec (cons name
-             (concat args child-blocks))))
+  (let [processed-children (reduce (fn [children [child-name & child-contents]]
+                                     (update children
+                                             child-name
+                                             #(conj (or % [])
+                                                    (vec child-contents))))
+                                   {}
+                                   child-blocks)]
+    [name (vec args) processed-children]))
 
 (def transform-options
-  {:data transform-data
+  {:data vector
    :0-indented-block transform-block
    :1-indented-block transform-block
    :2-indented-block transform-block
@@ -126,4 +68,12 @@
        (mapcat (fn [file]
                  (let [race (get-race-of-file file)
                        ships (-> file slurp parse)]
-                   (map #(assoc % :race race) ships))))))
+                   (map #(assoc-in % [2 "race"] race) ships))))))
+
+(comment
+  (->> data
+       (map first)
+       (reduce (fn [counts object]
+                 (update counts object #(inc (or % 0))))
+               {})
+       (sort-by last >)))
