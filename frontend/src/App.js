9fb0a9b199693ed257f0e6dc965f56405987499f
Vsevolod Romashov <7@7vn.ru>
2018-01-26 11:35:45 +0300

    extract the ships list to a separate component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,71 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Grid, Row, Col } from 'react-bootstrap';
 import R from 'ramda';
-import { Grid, Row, Col, Button, Collapse, Checkbox, Table } from 'react-bootstrap';
-import NumberFormat from 'react-number-format';
+
+import ShipsList from './ShipsList';
 import ShipPage from './ShipPage';
 import OutfitsList from './OutfitsList';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-const kebabCase = string => string.replace(/\s+/g, '-').toLowerCase();
-
-function LinkCell(props) {
-  return (
-    <td className="text-left">
-      <Link to={`/ships/${kebabCase(props.text)}`}>{props.text}</Link>
-    </td>
-  );
-}
-
-function TextCell(props) {
-  return (
-    <td className="text-left">
-      {props.text}
-    </td>
-  );
-}
-
-function RightCell(props) {
-  return (
-    <td className="text-right">
-      {props.children}
-    </td>
-  );
-}
-
-function FormattedNumber(props) {
-  return (
-    <NumberFormat value={props.number}
-                  displayType={'text'}
-                  thousandSeparator={true} />
-  );
-}
-
-function NumberCell(props) {
-  return (
-    <RightCell>
-      <FormattedNumber number={props.number} />
-    </RightCell>
-  );
-}
-
-function CrewAndBunks(props) {
-  if (props.crew > 0) {
-    return (
-      <RightCell>
-        <FormattedNumber number={props.crew} />
-        {' / '}
-        <FormattedNumber number={props.bunks} />
-      </RightCell>
-    );
-  } else {
-    return (<RightCell></RightCell>);
-  }
-}
-
 class App extends Component {
-  // TODO: compute this from this.state.data on the fly
+  kebabCase = string => string.replace(/\s+/g, '-').toLowerCase();
+
+  // TODO: compute this from this.state.ships on the fly
   races = [
     'human',
     'hai',
@@ -124,15 +72,15 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('/data.json').then(response => {
+    window.fetch('/data.json').then(response => {
       return response.json();
     }).then(data => {
-      this.setState({ isLoading: false, data: data });
+      this.setState({ isLoading: false, ships: data });
     });
   }
 
   toggleFiltersVisibility = () => {
-    this.setState({ filtersCollapsed: !this.state.filtersCollapsed })
+    this.setState({ filtersCollapsed: !this.state.filtersCollapsed });
   };
 
   toggleRaceFiltering = (race) => {
@@ -141,7 +89,7 @@ class App extends Component {
         this.state.raceFilter,
         { [race]: !this.state.raceFilter[race] }
       )
-    })
+    });
   };
 
   toggleCategoryFiltering = (category) => {
@@ -150,7 +98,7 @@ class App extends Component {
         this.state.categoryFilter,
         { [category]: !this.state.categoryFilter[category] }
       )
-    })
+    });
   }
 
   toggleLicenseFiltering = (license) => {
@@ -159,7 +107,7 @@ class App extends Component {
         this.state.licenseFilter,
         { [license]: !this.state.licenseFilter[license] }
       )
-    })
+    });
   }
 
   toggleOrdering = (columnName) => {
@@ -174,178 +122,6 @@ class App extends Component {
     }
   }
 
-  capitalize([first, ...rest]) {
-    return first.toUpperCase() + rest.join('').toLowerCase();
-  }
-
-  renderFilters() {
-    const raceCheckboxes = this.races.map(race => (
-      <Checkbox key={race}
-                checked={this.state.raceFilter[race]}
-                onChange={() => this.toggleRaceFiltering(race)}>
-        {this.capitalize(race)}
-      </Checkbox>
-    ));
-
-    const categoryCheckboxes = this.categories.map(category => (
-      <Checkbox key={category}
-                checked={this.state.categoryFilter[category]}
-                onChange={() => this.toggleCategoryFiltering(category)}>
-        {category}
-      </Checkbox>
-    ));
-
-    const licenseCheckboxes = Object.keys(this.licenses).map(license => (
-      <Checkbox key={license}
-                checked={this.state.licenseFilter[license]}
-                onChange={() => this.toggleLicenseFiltering(license)}>
-        {license}
-      </Checkbox>
-    ));
-
-    let collapseIcon;
-    if (this.state.filtersCollapsed) {
-      collapseIcon = <span className="glyphicon glyphicon-menu-down" />;
-    } else {
-      collapseIcon = <span className="glyphicon glyphicon-menu-up" />;
-    }
-
-    return (
-      <div className="filters-group">
-        <Collapse in={!this.state.filtersCollapsed}>
-          <Grid fluid={true}>
-            <Row>
-              <Col lg={1}>
-                <strong>Race</strong>
-                {raceCheckboxes}
-              </Col>
-              <Col lg={1}>
-                <strong>Category</strong>
-                {categoryCheckboxes}
-              </Col>
-              <Col lg={2}>
-                <strong>License</strong>
-                {licenseCheckboxes}
-              </Col>
-            </Row>
-          </Grid>
-        </Collapse>
-        <Button onClick={() => this.toggleFiltersVisibility()}>
-          Filters {collapseIcon}
-        </Button>
-      </div>
-    );
-  }
-
-  renderHeaders() {
-    const columns = [
-      ['Name', 'name'],
-      ['Race'],
-      ['Cost', 'cost'],
-      ['Category'],
-      ['Hull', 'hull'],
-      ['Shields', 'shields'],
-      ['Mass', 'mass'],
-      ['Engine cap.', 'engineCapacity'],
-      ['Weapon cap.', 'weaponCapacity'],
-      ['Fuel cap.', 'fuelCapacity'],
-      ['Outfit sp.', 'outfitSpace'],
-      ['Cargo sp.', 'cargoSpace'],
-      ['Crew / bunks', 'bunks'],
-      ['Licenses']
-    ]
-
-    return columns.map(([text, sortBy]) => {
-      let title, icon;
-
-      if (sortBy) {
-        title = <a className="table-header" onClick={() => this.toggleOrdering(sortBy)}>{text}</a>;
-
-        if (this.state.ordering.columnName === sortBy) {
-          if (this.state.ordering.order === 'asc') {
-            icon = <span className="glyphicon glyphicon-sort-by-attributes"></span>;
-          } else {
-            icon = <span className="glyphicon glyphicon-sort-by-attributes-alt"></span>;
-          }
-        }
-      } else {
-        title = text;
-      }
-
-      return (
-        <th className="text-center" key={text}>
-          {title}
-          {' '}
-          {icon}
-        </th>
-      );
-    });
-  }
-
-  renderLabel(text) {
-    const style = this.licenses[text] || text;
-    return (<span className={'label label-' + style} key={text}>{text}</span>);
-  }
-
-  renderLicenses(ship) {
-    return ship.licenses.map(
-      license => this.renderLabel(license)
-    ).reduce(
-      (licenses, license) => (licenses === null ? [license] : [...licenses, ' ', license]),
-      null
-    );
-  }
-
-  processedRows() {
-    const filters = [
-      ship => this.state.raceFilter[ship.race],
-      ship => this.state.categoryFilter[ship.category],
-      ship => R.none(license => !this.state.licenseFilter[license])(ship.licenses)
-    ];
-
-    const prop = R.propOr(0, this.state.ordering.columnName);
-    const sortedProp = (this.state.ordering.order === 'asc') ? R.ascend(prop) : R.descend(prop);
-    const comparator = R.sort(sortedProp);
-
-    return comparator(R.filter(R.allPass(filters), this.state.data));
-  }
-
-  renderRows() {
-    return this.processedRows().map(ship => (
-      <tr key={ship.name}>
-        <LinkCell text={ship.name} />
-        <TextCell text={this.renderLabel(ship.race)} />
-        <NumberCell number={ship.cost} />
-        <TextCell text={ship.category} />
-        <NumberCell number={ship.hull} />
-        <NumberCell number={ship.shields} />
-        <NumberCell number={ship.mass} />
-        <NumberCell number={ship.engineCapacity} />
-        <NumberCell number={ship.weaponCapacity} />
-        <NumberCell number={ship.fuelCapacity} />
-        <NumberCell number={ship.outfitSpace} />
-        <NumberCell number={ship.cargoSpace} />
-        <CrewAndBunks crew={ship.requiredCrew} bunks={ship.bunks} />
-        <TextCell text={this.renderLicenses(ship)} />
-      </tr>
-    ));
-  }
-
-  renderTable() {
-    return (
-      <Table striped bordered condensed hover>
-        <thead>
-          <tr>
-            {this.renderHeaders()}
-          </tr>
-        </thead>
-        <tbody>
-          {this.renderRows()}
-        </tbody>
-      </Table>
-    );
-  }
-
   render() {
     if (this.state.isLoading) {
       return (<div className="app">Loading...</div>);
@@ -355,25 +131,24 @@ class App extends Component {
           <Grid fluid={true}>
             <Row>
               <Col lg={12}>
-                <Route exact={true} path="/" render={() => (
-                  <div className="app">
-                    <ul className="nav nav-tabs">
-                      <li role="presentation" className="active">
-                        <Link to="/">Ships</Link>
-                      </li>
-                      <li role="presentation">
-                        <Link to="/outfits">Outfits</Link>
-                      </li>
-                    </ul>
-
-                    <ol className="breadcrumb top20"><li className="active">Ships</li></ol>
-
-                    {this.renderFilters()}
-                    {this.renderTable()}
-                  </div>
+                <Route exact={true} path="/" render={props => (
+                  <ShipsList ships={this.state.ships}
+                             races={this.races}
+                             categories={this.categories}
+                             licenses={this.licenses}
+                             filtersCollapsed={this.state.filtersCollapsed}
+                             ordering={this.state.ordering}
+                             raceFilter={this.state.raceFilter}
+                             categoryFilter={this.state.categoryFilter}
+                             licenseFilter={this.state.licenseFilter}
+                             toggleFiltersVisibility={this.toggleFiltersVisibility}
+                             toggleRaceFiltering={this.toggleRaceFiltering}
+                             toggleCategoryFiltering={this.toggleCategoryFiltering}
+                             toggleLicenseFiltering={this.toggleLicenseFiltering}
+                             toggleOrdering={this.toggleOrdering} />
                 )}/>
                 <Route path="/ships/:shipName" render={({ match }) => (
-                  <ShipPage ship={this.state.data.find(ship => kebabCase(ship.name) === match.params.shipName)}/>
+                  <ShipPage ship={this.state.ships.find(ship => this.kebabCase(ship.name) === match.params.shipName)}/>
                 )}/>
                 <Route path="/outfits" component={OutfitsList} />
               </Col>
