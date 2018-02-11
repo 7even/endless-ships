ec9d8357ec8927487e6ede77e8448df60188cdac
Vsevolod Romashov <7@7vn.ru>
2018-02-11 15:09:59 +0300

    add a new App component

---
+++ frontend/src/components/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import endlessShips from '../reducers';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { Grid, Row, Col } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Reference from './Reference';
+import Navigation from './Navigation';
+import ShipsList from './ShipsList';
+import ShipPage from './ShipPage';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+const App = () => (
+  <Provider store={createStore(endlessShips)}>
+    <Grid fluid={true}>
+      <Row>
+        <Col lg={12}>
+          <Reference>
+            <Router>
+              <div className="app">
+                <Navigation />
+                <Route exact={true} path="/" component={ShipsList} />
+                <Route path="/Ships/:shipName" component={ShipPage} />
+              </div>
+            </Router>
+          </Reference>
+        </Col>
+      </Row>
+    </Grid>
+  </Provider>
+);
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import './index.css';
 import 'github-fork-ribbon-css/gh-fork-ribbon.css';
 
