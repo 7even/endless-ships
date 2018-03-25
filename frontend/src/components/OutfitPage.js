aa596f6ebe6cf34bb4e740cf43811e1178094769
Vsevolod Romashov <7@7vn.ru>
2018-03-25 20:17:54 +0300

    refactor OutfitPage to use renderAttribute everywhere

--- frontend/src/components/OutfitPage.js
+++ frontend/src/components/OutfitPage.js
@@ -88,43 +88,43 @@ const OutfitPage = ({ outfit, shipInstallations }) => (
 
                   <Col md={4}>
                     <ul>
-                      <li>category: {outfit.category}</li>
-                      <li>cost: <FormattedNumber number={outfit.cost} /></li>
-                      <li>outfit space needed: <FormattedNumber number={outfit.outfitSpace} /></li>
-                      {outfit.weaponCapacity && <li>weapon capacity needed: <FormattedNumber number={outfit.weaponCapacity} /></li>}
-                      {outfit.engineCapacity && <li>engine capacity needed: <FormattedNumber number={outfit.engineCapacity} /></li>}
+                      {renderAttribute(outfit, R.prop('category'),       'category')}
+                      {renderAttribute(outfit, R.prop('cost'),           'cost')}
+                      {renderAttribute(outfit, R.prop('outfitSpace'),    'outfit space needed')}
+                      {renderAttribute(outfit, R.prop('weaponCapacity'), 'weapon capacity needed')}
+                      {renderAttribute(outfit, R.prop('engineCapacity'), 'engine capacity needed')}
                     </ul>
                   </Col>
 
                   <Col md={4}>
                     <ul>
-                      {outfit.thrust && <li>thrust: <FormattedNumber number={outfit.thrust} /></li>}
-                      {outfit.thrustingEnergy && <li>thrusting energy: <FormattedNumber number={outfit.thrustingEnergy} /></li>}
-                      {outfit.thrustingHeat && <li>thrusting heat: <FormattedNumber number={outfit.thrustingHeat} /></li>}
-                      {outfit.turn && <li>turn: <FormattedNumber number={outfit.turn} /></li>}
-                      {outfit.turningEnergy && <li>turning energy: <FormattedNumber number={outfit.turningEnergy} /></li>}
-                      {outfit.turningHeat && <li>turning heat: <FormattedNumber number={outfit.turningHeat} /></li>}
-                      {outfit.afterburnerFuel && <li>afterburner fuel: <FormattedNumber number={outfit.afterburnerFuel} /></li>}
-                      {outfit.afterburnerHeat && <li>afterburner heat: <FormattedNumber number={outfit.afterburnerHeat} /></li>}
-                      {outfit.afterburnerThrust && <li>afterburner thrust: <FormattedNumber number={outfit.afterburnerThrust} /></li>}
-                      {outfit.reverseThrust && <li>reverse thrust: <FormattedNumber number={outfit.reverseThrust} /></li>}
-                      {outfit.reverseThrustingEnergy && <li>reverse thrusting energy: <FormattedNumber number={outfit.reverseThrustingEnergy} /></li>}
-                      {outfit.reverseThrustingHeat && <li>reverse thrusting heat: <FormattedNumber number={outfit.reverseThrustingHeat} /></li>}
-                      {outfit.energyGeneration && <li>energy generation: <FormattedNumber number={outfit.energyGeneration} /></li>}
-                      {outfit.solarCollection && <li>solar collection: <FormattedNumber number={outfit.solarCollection} /></li>}
-                      {outfit.energyCapacity && <li>energy capacity: <FormattedNumber number={outfit.energyCapacity} /></li>}
-                      {outfit.energyConsumption && <li>energy consumption: <FormattedNumber number={outfit.energyConsumption} /></li>}
-                      {outfit.heatGeneration && <li>heat generation: <FormattedNumber number={outfit.heatGeneration} /></li>}
-                      {outfit.cooling && <li>cooling: <FormattedNumber number={outfit.cooling} /></li>}
-                      {outfit.activeCooling && <li>active cooling: <FormattedNumber number={outfit.activeCooling} /></li>}
-                      {outfit.coolingEnergy && <li>cooling energy: <FormattedNumber number={outfit.coolingEnergy} /></li>}
-                      {outfit.hullRepairRate && <li>hull repair rate: <FormattedNumber number={outfit.hullRepairRate} /></li>}
-                      {outfit.hullEnergy && <li>hull energy: <FormattedNumber number={outfit.hullEnergy} /></li>}
-                      {outfit.hullHeat && <li>hull heat: <FormattedNumber number={outfit.hullHeat} /></li>}
-                      {outfit.shieldGeneration && <li>shield generation: <FormattedNumber number={outfit.shieldGeneration} /></li>}
-                      {outfit.shieldEnergy && <li>shield energy: <FormattedNumber number={outfit.shieldEnergy} /></li>}
-                      {outfit.shieldHeat && <li>shield heat: <FormattedNumber number={outfit.shieldHeat} /></li>}
-                      {outfit.ramscoop && <li>ramscoop: <FormattedNumber number={outfit.ramscoop} /></li>}
+                      {renderAttribute(outfit, R.prop('thrust'),                 'thrust')}
+                      {renderAttribute(outfit, R.prop('thrustingEnergy'),        'thrusting energy')}
+                      {renderAttribute(outfit, R.prop('thrustingHeat'),          'thrusting heat')}
+                      {renderAttribute(outfit, R.prop('turn'),                   'turn')}
+                      {renderAttribute(outfit, R.prop('turningEnergy'),          'turning energy')}
+                      {renderAttribute(outfit, R.prop('turningHeat'),            'turning heat')}
+                      {renderAttribute(outfit, R.prop('afterburnerFuel'),        'afterburner fuel')}
+                      {renderAttribute(outfit, R.prop('afterburnerHeat'),        'afterburner heat')}
+                      {renderAttribute(outfit, R.prop('afterburnerThrust'),      'afterburner thrust')}
+                      {renderAttribute(outfit, R.prop('reverseThrust'),          'reverse thrust')}
+                      {renderAttribute(outfit, R.prop('reverseThrustingEnergy'), 'reverse thrusting energy')}
+                      {renderAttribute(outfit, R.prop('reverseThrustingHeat'),   'reverse thrusting heat')}
+                      {renderAttribute(outfit, R.prop('energyGeneration'),       'energy generation')}
+                      {renderAttribute(outfit, R.prop('solarCollection'),        'solar collection')}
+                      {renderAttribute(outfit, R.prop('energyCapacity'),         'energy capacity')}
+                      {renderAttribute(outfit, R.prop('energyConsumption'),      'energy consumption')}
+                      {renderAttribute(outfit, R.prop('heatGeneration'),         'heat generation')}
+                      {renderAttribute(outfit, R.prop('cooling'),                'cooling')}
+                      {renderAttribute(outfit, R.prop('activeCooling'),          'active cooling')}
+                      {renderAttribute(outfit, R.prop('coolingEnergy'),          'cooling energy')}
+                      {renderAttribute(outfit, R.prop('hullRepairRate'),         'hull repair rate')}
+                      {renderAttribute(outfit, R.prop('hullEnergy'),             'hull energy')}
+                      {renderAttribute(outfit, R.prop('hullHeat'),               'hull heat')}
+                      {renderAttribute(outfit, R.prop('shieldGeneration'),       'shield generation')}
+                      {renderAttribute(outfit, R.prop('shieldEnergy'),           'shield energy')}
+                      {renderAttribute(outfit, R.prop('shieldHeat'),             'shield heat')}
+                      {renderAttribute(outfit, R.prop('ramscoop'),               'ramscoop')}
 
                       {outfit.weapon && <WeaponAttributes weapon={outfit.weapon} />}
                     </ul>
